15cff3976452e36a049f21dbf9d808a45599f093
Froilan Sam Malibiran <froilan@ziphr.co>
2021-04-09 00:17:25 +0800

    fix: add catch

--- functions/auth.js
+++ functions/auth.js
@@ -1,22 +1,22 @@
-import api from "../utils/api";
+import axios from "axios";
 exports.handler = async function (event, context) {
   console.log("Event: ", event?.queryStringParameters?.code);
-  api
-    .post(
-      "https://identity.xero.com/connect/token",
-      {
-        grant_type: "authorization_code",
-        code: event?.queryStringParameters?.code,
-        redirect_uri: "https://facebook.com",
-      },
-      {
-        headers: {
-          Authorization:
-            "Basic MjNFQzZFMDJDMzgwNDUyNzkxNUQwRjc1NzZCNDEyM0M6VFNLcTY1QWFyUkNINTlzazdLVl9GeGJFY2IxYjNMeTZfankyQkp4dFVEMGZDWXlM",
-          "Content-Type": "application/json",
-        },
-      }
-    )
+
+  const params = new URLSearchParams();
+  params.append("grant_type", "authorization_code");
+  params.append("code", event?.queryStringParameters?.code);
+  params.append("redirect_uri", "https://facebook.com");
+
+  const config = {
+    headers: {
+      Authorization:
+        "Basic MjNFQzZFMDJDMzgwNDUyNzkxNUQwRjc1NzZCNDEyM0M6VFNLcTY1QWFyUkNINTlzazdLVl9GeGJFY2IxYjNMeTZfankyQkp4dFVEMGZDWXlM",
+      "Content-Type": "application/json",
+    },
+  };
+
+  axios
+    .post("https://identity.xero.com/connect/token", params, config)
     .then((data) => {
       console.log("Data: ", data);
     })
