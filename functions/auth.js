7c270eba222b81131eba5d7984cddd24322d4953
Froilan Sam Malibiran <froilan@ziphr.co>
2021-04-09 00:03:33 +0800

    feat: add axios

--- functions/auth.js
+++ functions/auth.js
@@ -1,4 +1,23 @@
+import api from "../utils/api";
 exports.handler = async function (event, context) {
-  console.log("Event: ", event);
-  console.log("context: ", context);
+  console.log("Event: ", event?.queryStringParameters?.code);
+  api
+    .post(
+      "https://identity.xero.com/connect/token",
+      {
+        grant_type: "authorization_code",
+        code: event?.queryStringParameters?.code,
+        redirect_uri: "https://facebook.com",
+      },
+      {
+        headers: {
+          Authorization:
+            "Basic MjNFQzZFMDJDMzgwNDUyNzkxNUQwRjc1NzZCNDEyM0M6VFNLcTY1QWFyUkNINTlzazdLVl9GeGJFY2IxYjNMeTZfankyQkp4dFVEMGZDWXlM",
+          "Content-Type": "application/json",
+        },
+      }
+    )
+    .then((data) => {
+      console.log("Data: ", data);
+    });
 };
---
+++ utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const timeout = 10000;
+
+const instance = axios.create({
+  timeout,
+});
+
+export default instance;
+
+export function attachToken(token) {
+  instance.defaults.headers.common.Authorization = `Bearer ${token}`;
+}
+
+export function detachToken() {
+  delete instance.defaults.headers.common.Authorization;
+}
