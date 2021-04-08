80e544c913449eb356626d389b3650b4279a3105
Froilan Sam Malibiran <froilan@ziphr.co>
2021-04-09 00:50:57 +0800

    fix: add catch

--- functions/auth.js
+++ functions/auth.js
@@ -1,29 +1,28 @@
 const axios = require("axios");
+const qs = require("qs");
+
 exports.handler = async function (event, context, callback) {
   console.log("Event: ", typeof event?.queryStringParameters?.code);
 
-  const params = new URLSearchParams();
-  params.append("grant_type", "authorization_code");
-  params.append("code", event?.queryStringParameters?.code);
-  params.append("redirect_uri", "https://facebook.com");
-
-  const config = {
-    headers: {
-      Authorization:
-        "Basic MjNFQzZFMDJDMzgwNDUyNzkxNUQwRjc1NzZCNDEyM0M6VFNLcTY1QWFyUkNINTlzazdLVl9GeGJFY2IxYjNMeTZfankyQkp4dFVEMGZDWXlM",
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
+  const params = {
+    grant_type: "authorization_code",
+    code: event?.queryStringParameters?.code,
+    redirect_uri: "https://facebook.com",
   };
 
-  console.log("Params: ", params);
+  const config = {
+    method: "POST",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      authorization:
+        "Basic MjNFQzZFMDJDMzgwNDUyNzkxNUQwRjc1NzZCNDEyM0M6VFNLcTY1QWFyUkNINTlzazdLVl9GeGJFY2IxYjNMeTZfankyQkp4dFVEMGZDWXlM",
+    },
+    data: qs.stringify(params),
+    url: "https://identity.xero.com/connect/token",
+  };
 
-  axios(
-    { url: "https://identity.xero.com/connect/token", method: "post" },
-    params,
-    config
-  )
+  axios(config)
     .then((data) => {
-      callback(null);
       console.log("Data: ", data);
     })
     .catch((err) => console.log("Error: ", err));
