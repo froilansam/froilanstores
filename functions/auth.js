bc2acc7630e701b47e1295fb8e994e6016e832c1
Froilan Sam Malibiran <froilan@ziphr.co>
2021-04-09 14:18:29 +0800

    fix: add catch

--- functions/auth.js
+++ functions/auth.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const qs = require("qs");
 const admin = require("firebase");
-const fetch = require("fetch");
+global.fetch = require("node-fetch");
 
 const { attachToken } = require("../utils/api");
 
