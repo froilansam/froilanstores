6bde8de54d2444266bcae8e5f13ea8b4e2ef5cfd
Froilan Sam Malibiran <froilan@ziphr.co>
2021-04-09 13:53:38 +0800

    fix: add catch

--- functions/auth.js
+++ functions/auth.js
@@ -1,5 +1,4 @@
 const axios = require("axios");
-const admin = require("firebase");
 const qs = require("qs");
 var admin = require("firebase-admin");
 var serviceAccount = require("../firebase_service_account.json");
